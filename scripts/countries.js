3e955a12e93d21f1f01e3f729fbf4dfc15f92eb1
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-25 23:09:31 +0600

    added specific country details

--- scripts/countries.js
+++ scripts/countries.js
@@ -5,20 +5,33 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    
-    const countriesContainer=document.getElementById('countries-container');
+
+    const countriesContainer = document.getElementById('countries-container');
     countries.forEach(country => {
-        
-        const countryDiv=document.createElement('div');
+
+        const countryDiv = document.createElement('div');
         countryDiv.classList.add('country-div')
-        countryDiv.innerHTML=`
+        countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
-        <h5>Capital: ${country.capital?country.capital[0]:'There is no capital in this country'}</h5>
-        <button onclick=loadDetail()>Details</button>
+        <h5>Capital: ${country.capital ? country.capital[0] : 'There is no capital in this country'}</h5>
+        <button onclick=loadDetail('${country.cca2}')>Details</button>
         `
         countriesContainer.appendChild(countryDiv)
     });
 }
-
+const loadDetail = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data))
+}
+const displayCountryDetail=country=>{
+    
+    const countryDetails=document.getElementById('country-details')
+    countryDetails.innerHTML=`
+    <h3>Name: ${country[0].name.common}</h3>
+    <img src="${country[0].flags.png}">
+    `
+}
 
 loadCountries()
