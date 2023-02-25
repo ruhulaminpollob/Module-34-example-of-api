5fe6f1d85ca8e368f66cab89225e9e7785035e3f
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-26 00:51:55 +0600

    dynamic food search site

--- scripts/meals.js
+++ scripts/meals.js
@@ -1,21 +1,23 @@
-const loadMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals = (searchMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`
+    fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
 }
 
 const displayMeals = meals => {
-    console.log(meals);
+    // console.log(meals);
     const mealsContainer = document.getElementById('meals-container');
+    mealsContainer.innerHTML='';
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal.strInstructions);
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col')
         mealDiv.innerHTML = `
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
             </div>
@@ -25,4 +27,10 @@ const displayMeals = meals => {
 
 }
 
-loadMeals()
+
+document.getElementById('meal-search-btn').addEventListener('click', function () {
+    const searchValue = document.getElementById('meal-search-filed').value;
+    loadMeals(searchValue)
+})
+
+loadMeals('rice')
