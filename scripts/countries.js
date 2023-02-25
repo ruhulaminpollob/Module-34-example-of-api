1df1541ddbbc2661a100cba5efff36c92eafb6f8
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-25 22:31:18 +0600

    added some styles

--- scripts/countries.js
+++ scripts/countries.js
@@ -8,11 +8,13 @@ const displayCountries = countries => {
     
     const countriesContainer=document.getElementById('countries-container');
     countries.forEach(country => {
-        console.log(country);
+        
         const countryDiv=document.createElement('div');
+        countryDiv.classList.add('country-div')
         countryDiv.innerHTML=`
         <h3>Name: ${country.name.common}</h3>
         <h5>Capital: ${country.capital?country.capital[0]:'There is no capital in this country'}</h5>
+        <button onclick=loadDetail()>Details</button>
         `
         countriesContainer.appendChild(countryDiv)
     });
